2c2c783e9817a4dc225c632ded6f8f2faf24cdfb
Marina Kovaleva <marinakovaleva259@gmail.com>
2021-03-09 22:32:09 +0200

    Implement portfolio block

---
+++ script/main.js
@@ -0,0 +1,9 @@
+function handleIn(e) {
+  $(this).find('.portfolio-content').removeClass('hidden')
+}
+
+function handleOut(e) {
+  $(this).find('.portfolio-content').addClass('hidden')
+}
+
+$('.portfolio-item').hover(handleIn, handleOut)
