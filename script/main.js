5e29b582db34d051db98a7d72fa3991a917ef571
Marina Kovaleva <marinakovaleva259@gmail.com>
2021-03-20 22:27:27 +0200

    Implement revisions

--- script/main.js
+++ script/main.js
@@ -10,6 +10,9 @@ $('.portfolio-item').hover(handleIn, handleOut)
 
 $(function () {
 
-  $('.reviews-slider').slick();
+  $('.reviews-slider').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.svg" alt="Arrow"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.svg" alt="Arrow"></button>'
+  });
 
 });
